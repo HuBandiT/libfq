030708af16e95fa77d4f37b0cf61e68f14aef957
HuBandiT <hubandit@gmail.com>
2026-06-30 14:45:50 +0200

    FQClear: do actually call _FQexecClearSQLDA to clear sqlda_in and sqlda_out

--- src/libfq.c
+++ src/libfq.c
@@ -4477,16 +4477,17 @@ FQclear(FBresult *result)
 
 	/*
 	 * NOTE: these should be cleared by _FQexecClearResult() anyway
-	 * XXX we should call _FQexecClearSQLDA here too
 	 */
 	if (result->sqlda_in != NULL)
 	{
+		_FQexecClearSQLDA(result->sqlda_in);
 		free(result->sqlda_in);
 		result->sqlda_in = NULL;
 	}
 
 	if (result->sqlda_out != NULL)
 	{
+		_FQexecClearSQLDA(result->sqlda_out);
 		free(result->sqlda_out);
 		result->sqlda_out  = NULL;
 	}
